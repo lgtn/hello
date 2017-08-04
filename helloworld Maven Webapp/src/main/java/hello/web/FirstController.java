cdb089d73f3b36f764988927d2566b1b35f2814c
lgtn <237961657@qq.com>
2017-08-04 16:49:06 +0800

    添加logback日志支持

--- helloworld Maven Webapp/src/main/java/hello/web/FirstController.java
+++ helloworld Maven Webapp/src/main/java/hello/web/FirstController.java
@@ -1,14 +1,23 @@
 package hello.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
+	
+	private static Logger log = LoggerFactory.getLogger(FirstController.class);
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String home() {
+		
+		log.info("....hello...");
+		
+		log.info("测试日志格式化...{}...成功！！！", "你好，Spring Boot");
+		
 		return "你好，Spring Boot";
 	}
 
